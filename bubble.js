95388aa7649e1e5274129eca2d1cc22c97f9335c
Bharadwaja sahoo <155898711+Bharadwaja-sahoo@users.noreply.github.com>
2024-03-04 08:39:49 +0530

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -17,6 +17,7 @@ var hitrn=0;
         document.querySelector("#hitVal").textContent=hitrn;
     }
     
+    
     function makeBubble(){
         var clutter="";
     for(var i=1;i<=102;i++){
@@ -52,55 +53,3 @@ var hitrn=0;
     makeBubble();
     getNewHit();
 }
-
-
-// var timer=60;
-// var score=0;
-// var hitrn=0;
-
-
-// function increaseScore(){
-//     score += 10;
-//     document.querySelector("#scoreVal").textContent=score;
-// }
-
-// function getNewHit(){
-//     hitrn=Math.floor(Math.random()*10);
-//     document.querySelector("#hitVal").textContent=hitrn;
-// }
-
-// function makeBubble(){
-//     var clutter="";
-// for(var i=1;i<=102;i++){
-//     var rn=Math.floor(Math.random()*10)
-//     clutter  +=`<div class="bubble">${rn}</div>`;
-// }
-
-// document.querySelector("#pbtm").innerHTML=clutter;
-// }
-
-// function runTimer(){
-//     var timerInt = setInterval(function (){
-//         if(timer>0){
-//         timer--;
-//         document.querySelector("#timerValue").textContent=timer;
-//         }
-//         else{
-//             clearInterval(timerInt);
-//             document.querySelector("#pbtm").innerHTML=`Score:${score}<br>GAME OVER`
-//         }
-//     },1000);
-// }
-
-// document.querySelector("#pbtm").addEventListener("click",function(details){
-//     var clickedNum=Number(details.target.textContent);
-//     if(clickedNum==hitrn){
-//         increaseScore();
-//         makeBubble();
-//         getNewHit();
-//     }
-// })
-
-// runTimer();
-// makeBubble();
-// getNewHit();
